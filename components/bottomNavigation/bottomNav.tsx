7bcb98aeb58e199238e10d8403446ff9d7fff4fb
Mjd Thif <mjd.thif@icloud.com>
2022-09-18 14:05:55 +0200

    fixed the map iterator error

--- components/bottomNavigation/bottomNav.tsx
+++ components/bottomNavigation/bottomNav.tsx
@@ -10,6 +10,7 @@ import { FcBusinessContact } from "react-icons/fc";
 import style from "./bottomNav.module.scss";
 
 export interface LinkData {
+  id: number;
   linkName: string;
   linkHref: string;
   icon: ReactNode;
@@ -17,21 +18,25 @@ export interface LinkData {
 
 export const links: LinkData[] = [
   {
+    id: 1,
     linkName: "Home",
     linkHref: "/",
     icon: <BiHomeAlt size={20} />,
   },
   {
+    id: 2,
     linkName: "Services",
     linkHref: "/services",
     icon: <AiFillCustomerService size={20} />,
   },
   {
+    id: 3,
     linkName: "About",
     linkHref: "/about",
     icon: <FcAbout size={20} />,
   },
   {
+    id: 4,
     linkName: "Contact",
     linkHref: "/contact",
     icon: <FcBusinessContact size={20} />,
@@ -41,8 +46,8 @@ export const links: LinkData[] = [
 const BottomNav = () => {
   return (
     <div className={style.container}>
-      {links.map((item: LinkData, index: number) => (
-        <NavLink {...item} key={index} />
+      {links.map((item: LinkData) => (
+        <NavLink {...item} key={item.id} />
       ))}
     </div>
   );
